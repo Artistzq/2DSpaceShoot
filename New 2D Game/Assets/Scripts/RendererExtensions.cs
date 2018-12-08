4af61f5c0ea77a26740411e3cd4730cfd5a57365
Azq <2237065231@qq.com>
2018-12-09 00:12:37 +0800

    添加了滚动
    滚动添加完成；
    场景无限写了一半

---
+++ New 2D Game/Assets/Scripts/RendererExtensions.cs
@@ -0,0 +1,18 @@
+﻿/** 
+ *负责人:
+ *版本:
+ *提交日期:
+ *功能描述:  
+ *修改记录: 
+*/  
+
+using UnityEngine;
+
+public static class RendererExtensions
+{
+	public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
+	{
+		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+		return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+	}
+}
--- New 2D Game/Assets/Scripts/ScrollingScript.cs
+++ New 2D Game/Assets/Scripts/ScrollingScript.cs
@@ -26,7 +26,42 @@ public class ScrollingScript : MonoBehaviour
 	//默认不加在摄像机上，所以设置为否(false)
 	public bool isLinkedToCamera = false;
 
-	void Start () 
+	//1.背景无限
+	public bool isLooping = false;
+
+	//2.渲染器子类的一个列表
+	private List<SpriteRenderer> backgroundPart;
+
+	//3.获取所有子类
+	private void Start() 
+	{
+		//只让背景无限
+		if (isLooping)
+		{
+			//获取所有含有渲染器的这一层的子类
+			backgroundPart = new List<SpriteRenderer>();
+
+			for(int i=0; i < transform.childCount; i++)
+			{
+				Transform child = transform.GetChild(i);
+				SpriteRenderer r = child.GetComponent<SpriteRenderer>();
+
+				//只添加可见的子类
+				if (r != null)
+					backgroundPart.Add(r);
+			}	
+
+			//按位置排序
+			//笔记: 从左到右获取子类
+			//我们需要添加一点情形
+			//去搞定所有可能的滚动方向
+			backgroundPart = backgroundPart.OrderBy(
+				t => t.transform.position.x
+			).ToList();
+		}
+	}
+
+	void Update () 
 	{
 		//移动
 		Vector3 movement = new Vector3(
@@ -39,7 +74,18 @@ public class ScrollingScript : MonoBehaviour
 		transform.Translate(movement);
 		
 		//移动摄像机
+		//如果与摄像机相链接，if执行，则摄像机也移动
+		//前景与摄像机相链接
 		if (isLinkedToCamera)
 			Camera.main.transform.Translate(movement);
+
+		//4.循环
+		if (isLooping)
+		{
+			//获取第一个物体
+			//这个列表从左到右
+			Transform firstChild = backgroundPart.FirstOrDefault();
+
+		}
 	}
 }
